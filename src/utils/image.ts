fdd5e3e71337f3bb2912f0ea329fee738436a5c4
Justin Seibert <justinseibert@users.noreply.github.com>
2023-12-06 15:31:28 -0800

    fix: replace calipers with image-size

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,12 +1,13 @@
-import Calipers from 'calipers'
-import { Result as CalipersResult } from 'calipers/src'
-const calipers = Calipers('png', 'jpeg', 'webp')
+import imageSize from 'image-size'
 
 export const measureImage = async (image: string) => {
    return new Promise((resolve, reject) => {
-      calipers
-         .measure(image)
-         .then((result: CalipersResult) => resolve(result))
-         .catch((err: Error) => reject(err))
+      imageSize(image, (err, dimensions) => {
+         if (err) {
+            reject(err)
+         } else {
+            resolve(dimensions)
+         }
+      })
    })
 }
