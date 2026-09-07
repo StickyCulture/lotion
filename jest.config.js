075a92413c64a10483cd780c45353efc8ff4753d
Justin Seibert <justinseibert@users.noreply.github.com>
2026-09-07 11:30:58 -0700

    test: fixes jest import issues

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
    preset: 'ts-jest',
    testEnvironment: 'node',
    transform: {
-      '^.+.tsx?$': ['ts-jest', {}],
+      '^.+\\.tsx?$': ['ts-jest', {}],
+      '^.+\\.jsx?$': 'babel-jest',
    },
+   transformIgnorePatterns: ['/node_modules/(?!@faker-js/faker)'],
 }
